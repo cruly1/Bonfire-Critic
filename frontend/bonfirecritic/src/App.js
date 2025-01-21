ddb98f09fa51d8854ca2a98c879f3c260533816a
cruly1 <pavai.viktor2@gmail.com>
2025-01-21 22:02:39 +0100

    Updated App.js according to the new functions.

--- frontend/bonfirecritic/src/App.js
+++ frontend/bonfirecritic/src/App.js
@@ -5,6 +5,17 @@ import Navbar from './components/Navbar/Navbar.js';
 import Footer from './components/Footer/Footer.js';
 import Home from './components/Pages/Home/Home.js';
 import Games from './components/Pages/Games/Games.js';
+import Login from './components/Pages/Login/Login.js';
+import Register from './components/Pages/Register/Register.js';
+import GameDetails from './components/Pages/Games/GameDetails/GameDetails.js';
+
+const sampleGame = {
+    title: 'The Legend of Bonfire',
+    description: 'An epic adventure game where you gather resources, build bonfires, and battle mythical creatures.',
+    rating: 4.8,
+    favorites: 245,
+    releaseDate: '2023-05-01',
+};
 
 function App() {
     return (
@@ -14,7 +25,9 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/games" element={<Games />} />
-                    <Route path="/login" element={<div>Login/Register Page</div>} />
+                    <Route path="/games/:id" element={<GameDetails />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
                 </Routes>
                 <Footer />
             </div>
